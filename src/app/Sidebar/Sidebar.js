3c68f08e3917aa0cfb48e72454b0a625a223ee0c
RohanMagar7 <rmtechyt@gmail.com>
2023-12-03 23:11:12 +0530

    add store in sidbar js file

--- src/app/Sidebar/Sidebar.js
+++ src/app/Sidebar/Sidebar.js
@@ -8,7 +8,7 @@ function Sidebar({handleChange}) {
     <>
       <section className="sidebar">
         <div className="logo-container">
-          <h1>#</h1>
+          <h1>Store</h1>
         </div>
         <Category handleChange={handleChange} />
         <Price handleChange={handleChange} />
